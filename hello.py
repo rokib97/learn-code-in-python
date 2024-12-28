3b8a1332dae783b54294fec1105af489f057f3ab
rokib97 <rokibulhasan.ph@gmail.com>
2024-12-28 22:06:04 +0600

    variable and data type

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello Python...")
+print("Hello Python again...")
+print("Hello Python again and again...")
---
+++ variable.py
@@ -0,0 +1,93 @@
+# variable can be used to store data values   
+player_health = 1000
+player_health = 900
+player_health = 800 
+player_health = 700
+player_health = 600
+# print(player_health)
+
+# Storing Results of an Expression
+player_health = 1000
+armor_multiplier = 2
+armored_health = player_health * armor_multiplier
+# print(armored_health)
+
+# Negative Numbers
+player_health = 100
+poison_damage = -10
+
+player_poison_health = player_health + poison_damage
+# print(player_poison_health)
+
+
+# Comments
+#single line comment
+# This is a comment
+"""
+Comments are used to explain code when the code is not self-explanatory.
+Comments are ignored by Python interpreter.
+Comments can be used to prevent execution when testing code.
+Comments can be used to make the code more readable.
+Comments can be used to prevent execution when testing code.
+
+"""
+# print("Hello Python...") # This is a comment
+
+
+# Basic Variable Types
+# string 
+my_name = "John"
+
+# number
+x = 10
+y = -10
+z = 10.1
+u = -10.1
+
+#boolean 
+is_student = True
+is_admin = False
+
+
+
+# F-strings in Python
+name = "Rokibul Hasan"
+age = 25
+race = "dwarf"
+# print(f"{name} is a {race} who is {age} years old.")
+
+
+# NoneType Variables
+# None is a special constant in Python that represents the absence of a value or a null value.
+enemy = None
+# print(enemy)
+
+# Dynamic Typing
+x = 5
+x = 'five'
+x = True
+# print(x)
+
+
+# Math With Strings || srting concatenation
+sentence_start = "You have "
+sentence_end = " health"
+player1_health = "1200"
+player2_health = "1100"
+# print(sentence_start + player1_health + sentence_end)
+# print(sentence_start + player2_health + sentence_end)
+
+
+# Multi-Variable Declaration
+sword_name, sward_damage, sward_length = "Excalibur", 100, 200
+print(sword_name, sward_damage, sward_length)
+
+
+# avg  calculator
+game_one_score = 97
+game_two_score = 91
+game_three_score = 106
+game_four_score = 105
+total = game_one_score + game_two_score + game_three_score + game_four_score
+average_score = total / 4
+print(round(average_score))
