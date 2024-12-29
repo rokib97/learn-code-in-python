1e74338cf4b47c59f48d088df3416fdde767082b
rokib97 <rokibulhasan.ph@gmail.com>
2024-12-29 18:10:37 +0600

    playing with numbers

---
+++ py_numbers.py
@@ -0,0 +1,62 @@
+# Description: This file contains examples of how to use numbers in Python
+#Calculating Damage
+def calculate_damage(sword, arrow,spear,dagger, fireball):
+    total_damage = sword + arrow + spear + dagger + fireball
+    average_damage = total_damage / 5
+    return total_damage, average_damage
+
+total_damage, average_damage = calculate_damage(10, 15, 20, 25, 30)
+# print(f"Total damage: {total_damage}, Average damage: {average_damage}")
+
+# Normal Division vs Floor Division
+# Normal Division Return a float
+# Floor Division Return an integer
+print(4/2) 
+print(5//2)
+
+# Exponents
+# read as 2 to the power of 3
+
+print(2 ** 3) # 2 * 2 * 2 = 8
+print(2 ** 4) # 2 * 2 * 2 * 2 = 16
+print(2 ** 5) # 2 * 2 * 2 * 2 * 2 = 32    
+
+
+# Changing In Place
+def update_player_score(current_score, increment):
+    current_score = current_score + increment
+    # current_score += increment # This is the same as the line above
+    return current_score
+
+
+# Plus Equals
+star_rating = 4
+star_rating += 1
+
+# Minus Equals
+health = 100
+health -= 10
+
+# Multiply Equals
+damage = 20
+damage *= 2
+
+# Divide Equals
+mana = 100
+mana /= 2
+
+# Modulus Equals
+# Modulus is the remainder of a division operation
+# 5 % 2 = 1
+# 5 / 2 = 2 remainder 1
+# 5 % 3 = 2
+# 5 / 3 = 1 remainder 2
+# 5 % 5 = 0
+# 5 / 5 = 1 remainder 0
+
+
+def get_hurt(current_health,damage):
+    current_health -= damage
+    return current_health
+
+# print(get_hurt(100, 10))
